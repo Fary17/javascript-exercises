2df8e96a4759c584e029fc2609ad787466da5133
Faryal Ilyas <faryal.ilyas@stud.th-luebeck.de>
2025-06-29 23:46:47 +0200

    einfach rein

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let result = '';
+    i = 0;
+    while (i < number) {
+        result += string;
+        i++;
+    }
+    return result;
 };
+
+console.log(repeatString('hey', 3)); // heyheyhey
 
 // Do not edit below this line
 module.exports = repeatString;
